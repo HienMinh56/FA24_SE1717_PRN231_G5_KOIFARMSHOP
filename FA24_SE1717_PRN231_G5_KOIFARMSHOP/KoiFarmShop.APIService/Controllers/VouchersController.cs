4a0dc0284d36bd9480b8d119b6c58ca07451623b
Phan Minh Tùng <110610398+tungphan1011@users.noreply.github.com>
2024-09-19 21:04:33 +0700

    voucher api

---
+++ FA24_SE1717_PRN231_G5_KOIFARMSHOP/KoiFarmShop.APIService/Controllers/VouchersController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using KoiFarmShop.Data.Models;
+using KoiFarmShop.Service;
+using KoiFarmShop.Service.Base;
+
+namespace KoiFarmShop.APIService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VouchersController : ControllerBase
+    {
+        private readonly VoucherService _voucherService;
+
+        public VouchersController()
+        {
+            _voucherService ??= new VoucherService();
+        }
+
+        // GET: api/Vouchers
+        [HttpGet]
+        public async Task<IBusinessResult> GetVouchers()
+        {
+            return await _voucherService.GetAll();
+        }
+
+        // GET: api/Vouchers/5
+        [HttpGet("{id}")]
+        public async Task<IBusinessResult> GetVoucher(int id)
+        {
+            var voucher = await _context.Vouchers.FindAsync(id);
+
+            if (voucher == null)
+            {
+                return NotFound();
+            }
+
+            return voucher;
+        }
+
+        // PUT: api/Vouchers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IBusinessResult> PutVoucher(int id, Voucher voucher)
+        {
+            if (id != voucher.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(voucher).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VoucherExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Vouchers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Voucher>> PostVoucher(Voucher voucher)
+        {
+            _context.Vouchers.Add(voucher);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetVoucher", new { id = voucher.Id }, voucher);
+        }
+
+        // DELETE: api/Vouchers/5
+        [HttpDelete("{id}")]
+        public async Task<IBusinessResult> DeleteVoucher(int id)
+        {
+            var voucher = await _context.Vouchers.FindAsync(id);
+            if (voucher == null)
+            {
+                return NotFound();
+            }
+
+            _context.Vouchers.Remove(voucher);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+    }
+}
