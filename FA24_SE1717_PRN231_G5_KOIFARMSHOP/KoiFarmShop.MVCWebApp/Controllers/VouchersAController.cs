c30ab5ea73be7627784f4a7431c2e96f5c7c2fc9
tungphan1011 <gametungphan10@gmail.com>
2024-10-17 15:57:33 +0700

    ajax

---
+++ FA24_SE1717_PRN231_G5_KOIFARMSHOP/KoiFarmShop.MVCWebApp/Controllers/VouchersAController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KoiFarmShop.MVCWebApp.Controllers
+{
+    public class VouchersAController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
