aa5cb46416dfd8d06218223d7331224cc018020d
tungphan1011 <gametungphan10@gmail.com>
2024-10-20 21:05:29 +0700

    update order and voucher

--- FA24_SE1717_PRN231_G5_KOIFARMSHOP/KoiFarmShop.Data/Request/CreateVoucherRequest.cs
+++ FA24_SE1717_PRN231_G5_KOIFARMSHOP/KoiFarmShop.Data/Request/CreateVoucherRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,15 +12,47 @@ namespace KoiFarmShop.Data.Request
         public string VoucherId { get; set; }
 
         public string VoucherCode { get; set; }
-
+        [Range(0.1, 1)]
         public double DiscountAmount { get; set; }
-
+        [Range(0, 100)]
         public double MinOrderAmount { get; set; }
 
         public int Status { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime? ValidityStartDate { get; set; }
 
+        [DataType(DataType.Date)]
+        [EndDateGreaterThanStartDate("ValidityStartDate", ErrorMessage = "Validity end date must be greater than start date.")]
         public DateTime? ValidityEndDate { get; set; }
+    }
+
+    public class EndDateGreaterThanStartDate : ValidationAttribute
+    {
+        private readonly string _startDatePropertyName;
+
+        public EndDateGreaterThanStartDate(string startDatePropertyName)
+        {
+            _startDatePropertyName = startDatePropertyName;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var startDateProperty = validationContext.ObjectType.GetProperty(_startDatePropertyName);
+            if (startDateProperty == null)
+            {
+                return new ValidationResult($"Unknown property: {_startDatePropertyName}");
+            }
+
+            var startDateValue = (DateTime?)startDateProperty.GetValue(validationContext.ObjectInstance);
+            var endDateValue = (DateTime?)value;
+
+            if (startDateValue.HasValue && endDateValue.HasValue && endDateValue <= startDateValue)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
     }
 }
