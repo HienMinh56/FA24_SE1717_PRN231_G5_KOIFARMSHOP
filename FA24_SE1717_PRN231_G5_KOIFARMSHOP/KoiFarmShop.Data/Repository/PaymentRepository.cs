82a4bc78d20b64bca86f146adff0908aa8135d97
NguyenDucLinh411 <linhnguyen41103@gmail.com>
2024-11-01 08:17:50 +0700

    fix/payment detail

--- FA24_SE1717_PRN231_G5_KOIFARMSHOP/KoiFarmShop.Data/Repository/PaymentRepository.cs
+++ FA24_SE1717_PRN231_G5_KOIFARMSHOP/KoiFarmShop.Data/Repository/PaymentRepository.cs
@@ -1,7 +1,6 @@
 ﻿using KoiFarmShop.Data.Base;
 using KoiFarmShop.Data.Models;
 using KoiFarmShop.Data.Request;
-using KoiFarmShop.Data.Request;
 using Microsoft.EntityFrameworkCore;
 
 namespace KoiFarmShop.Data.Repository
@@ -13,5 +12,12 @@ namespace KoiFarmShop.Data.Repository
         }
         public PaymentRepository(FA24_SE1717_PRN231_G5_KOIFARMSHOPContext context) => _context = context;
 
+        public async Task<Payment> GetPaymentByIdAsync(string paymentId)
+        {
+            return await _context.Payments.Include(o => o.Orders)
+                .Include(c => c.Consignments)
+                .Include(u => u.User)
+                .FirstOrDefaultAsync(c => c.PaymentId == paymentId);
+        }
     }
 }
